5ed213deab8f924bb97591bc1d533c22425496ec
seonghyun <seonghyun@seonghyun-PC>
2015-06-16 07:52:36 +0900

    controller 기본추가

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/MemberController.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/MemberController.java
@@ -1,13 +1,27 @@
 package kr.co.ohdeokrionline.controller;
 
+import kr.co.ohdeokrionline.model.dao.Message_Dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
+	@Autowired
+	private SqlSession sqlSession;
+
+	
+
 	@RequestMapping("login.five")
-	public String login(){
-		
+	public String login() {
+
 		return "login/login";
+	}
+	
+	@RequestMapping("test.message")
+	public void insert() {
+		Message_Dao dao = sqlSession.getMapper(Message_Dao.class);
 	}
 }
