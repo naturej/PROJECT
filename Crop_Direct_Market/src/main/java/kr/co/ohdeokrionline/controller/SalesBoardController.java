cf4732c3acb73d067e6358101cd4b53a21c726ab
naturej <natu_rej@naver.com>
2015-06-16 18:31:30 +0900

    Message

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
@@ -1,6 +1,7 @@
 package kr.co.ohdeokrionline.controller;
 
 import kr.co.ohdeokrionline.model.dao.Message_Dao;
+import kr.co.ohdeokrionline.model.vo.Message_DTO;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +15,51 @@ public class SalesBoardController {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	@RequestMapping("list")
-	public String messageList(Model model){
+	// 받은 메세지함
+	@RequestMapping("receiveList")
+	public String receiveList(Model model){
 		
 		Message_Dao dao =  sqlSession.getMapper(Message_Dao.class);
 		model.addAttribute("list",dao.ListRec());
 		
-		return "list";
+		return "receivelist";
 	}
 	
-	@RequestMapping("send")
-	public void insert() {
-		Message_Dao dao = sqlSession.getMapper(Message_Dao.class);
+	// 보낸 메세지함
+	@RequestMapping("sendList")
+	public String sendList(Model model){
+		
+		Message_Dao dao =  sqlSession.getMapper(Message_Dao.class);
+		model.addAttribute("list",dao.ListSend());
+		
+		return "sendlist";
 	}
+	
+	// 메세지 보내기
+	@RequestMapping("sendMessage")
+	public String insert(Message_DTO message) {
+		Message_Dao dao = sqlSession.getMapper(Message_Dao.class);
+		dao.insertRec(message);
+		dao.insertSend(message);
+		
+		return "redirect:sendList";
+	}
+	
+	// 보낸 메세지 삭제
+	@RequestMapping("deleteSend")
+	public String deleteSend(String seq){
+		Message_Dao dao = sqlSession.getMapper(Message_Dao.class);
+		dao.deleteSend(seq);
+		
+		return "redirect:sendList";
+	}
+	
+	// 받은 메세지 삭제
+		@RequestMapping("deleteRec")
+		public String deleteRec(String seq){
+			Message_Dao dao = sqlSession.getMapper(Message_Dao.class);
+			dao.deleteRec(seq);
+			
+			return "redirect:sendRec";
+		}
 }
--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/dao/Message_Dao.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/dao/Message_Dao.java
@@ -5,7 +5,16 @@ import java.util.ArrayList;
 import kr.co.ohdeokrionline.model.vo.Message_DTO;
 
 public interface Message_Dao {
+	// 받은 메세지 목록
 	public ArrayList<Message_DTO> ListRec();
+	// 보낸 메세지 목록
 	public ArrayList<Message_DTO> ListSend();
-	void insertMessage(Message_DTO message);
+	// 메세지 작성
+	public void insertRec(Message_DTO message);
+	// 메세지 작성
+	public void insertSend(Message_DTO message);
+	// 받은 메세지 삭제
+	public int deleteRec(String seq);
+	// 보낸 메세지 삭제
+	public int deleteSend(String seq);
 }
