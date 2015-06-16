9bcb458001e4ab70b25db6a10bf9b57763e3a582
moonseonghyun <gathering11@naver.com>
2015-06-16 09:56:02 +0900

    1

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
@@ -1,5 +1,17 @@
 package kr.co.ohdeokrionline.controller;
 
-public class SalesBoardController {
+import kr.co.ohdeokrionline.model.dao.Message_Dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class SalesBoardController {
+	@Autowired
+	private SqlSession sqlSession;
+
+	@RequestMapping("test.message")
+	public void insert() {
+		Message_Dao dao = sqlSession.getMapper(Message_Dao.class);
+	}
 }
