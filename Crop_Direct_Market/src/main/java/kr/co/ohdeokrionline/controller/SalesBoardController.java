7d6444747e833f322c4a239be32f463e637d0855
naturej <natu_rej@naver.com>
2015-06-16 18:47:57 +0900

    Controller 수정

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/message/")
+@RequestMapping("/marketplace/")
 public class SalesBoardController {
 	@Autowired
 	private SqlSession sqlSession;
