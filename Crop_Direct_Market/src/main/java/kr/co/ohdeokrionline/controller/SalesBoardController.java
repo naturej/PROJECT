b24235df56aaaf2428f9457890327298d273df18
naturej <natu_rej@naver.com>
2015-06-16 17:36:14 +0900

    Message수정중..

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
@@ -4,13 +4,26 @@ import kr.co.ohdeokrionline.model.dao.Message_Dao;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/message/")
 public class SalesBoardController {
 	@Autowired
 	private SqlSession sqlSession;
-
-	@RequestMapping("test.message")
+	
+	@RequestMapping("list")
+	public String messageList(Model model){
+		
+		Message_Dao dao =  sqlSession.getMapper(Message_Dao.class);
+		model.addAttribute("list",dao.ListRec());
+		
+		return "list";
+	}
+	
+	@RequestMapping("send")
 	public void insert() {
 		Message_Dao dao = sqlSession.getMapper(Message_Dao.class);
 	}
--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/dao/Message_Dao.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/dao/Message_Dao.java
@@ -1,7 +1,11 @@
 package kr.co.ohdeokrionline.model.dao;
 
+import java.util.ArrayList;
+
 import kr.co.ohdeokrionline.model.vo.Message_DTO;
 
 public interface Message_Dao {
+	public ArrayList<Message_DTO> ListRec();
+	public ArrayList<Message_DTO> ListSend();
 	void insertMessage(Message_DTO message);
 }
