ad21e15ef2012728c648cfe8d708c2e982f3da45
naturej <natu_rej@naver.com>
2015-06-16 11:51:04 +0900

    Message_DTO, Message_Dao.xml 수정

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/vo/Message_DTO.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/vo/Message_DTO.java
@@ -1,5 +1,50 @@
 package kr.co.ohdeokrionline.model.vo;
 
-public class Message_DTO {
+import java.sql.Date;
 
+public class Message_DTO {
+	private int me_num;
+	private String me_subject;
+	private String me_content;
+	private Date me_date;
+	private String send_userid;
+	private String rec_userid;
+	
+	public int getMe_num() {
+		return me_num;
+	}
+	public void setMe_num(int me_num) {
+		this.me_num = me_num;
+	}
+	public String getMe_subject() {
+		return me_subject;
+	}
+	public void setMe_subject(String me_subject) {
+		this.me_subject = me_subject;
+	}
+	public String getMe_content() {
+		return me_content;
+	}
+	public void setMe_content(String me_content) {
+		this.me_content = me_content;
+	}
+	public Date getMe_date() {
+		return me_date;
+	}
+	public void setMe_date(Date me_date) {
+		this.me_date = me_date;
+	}
+	public String getSend_userid() {
+		return send_userid;
+	}
+	public void setSend_userid(String send_userid) {
+		this.send_userid = send_userid;
+	}
+	public String getRec_userid() {
+		return rec_userid;
+	}
+	public void setRec_userid(String rec_userid) {
+		this.rec_userid = rec_userid;
+	}
+	
 }
