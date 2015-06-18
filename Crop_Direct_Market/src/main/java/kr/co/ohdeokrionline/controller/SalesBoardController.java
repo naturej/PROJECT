aeb787f0c2010ecdd9041b2e0918633169863e17
kosta <kosta@kosta-HP>
2015-06-18 19:02:30 +0900

    판매게시판

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/controller/SalesBoardController.java
@@ -1,12 +1,23 @@
 package kr.co.ohdeokrionline.controller;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import kr.co.ohdeokrionline.model.dao.Enuri_Dao;
 import kr.co.ohdeokrionline.model.dao.Message_Dao;
 import kr.co.ohdeokrionline.model.dao.SaleBoard_Dao;
+import kr.co.ohdeokrionline.model.dao.SalesBoard_Dao;
 import kr.co.ohdeokrionline.model.vo.Enuri_DTO;
 import kr.co.ohdeokrionline.model.vo.Message_DTO;
+import kr.co.ohdeokrionline.model.vo.SalesBoard_DTO;
+import kr.co.ohdeokrionline.model.vo.Unit_DTO;
+import net.sf.json.JSONArray;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +25,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Controller
 public class SalesBoardController {
@@ -22,7 +35,130 @@ public class SalesBoardController {
 	
 //	salesboard 완성된 이후
 //	HashMap<String, String> input = new HashMap<String, String>();
+	//판매글 리스트 출력
+	@RequestMapping("salboardlist.five")
+		public String salboardlist(String pg,Model model) throws ClassNotFoundException, SQLException{
+ 	//(String pg , String f , String q , Model model)
+ 	int page = 1;
+	//String field = "TITLE";
+	//String query ="%%";
+	if(pg != null && !pg.equals("")){
+		page = Integer.parseInt(pg);
+	}
+	//if(f != null && !f.equals("")){
+	//	field = f;
+	//}
+	//if(q != null && !q.equals("")){
+	//	query = q;
+	//}
+	//System.out.println(pg+" / "+f+" / "+q);
+	System.out.println(page);
+	SalesBoard_Dao salboardDao= sqlSession.getMapper(SalesBoard_Dao.class);
+	//System.out.println(pg+" / "+f+" / "+q);
+	//List<Board_DTO> list = boardDao.getBoardlist(page, field, query);
+	List<SalesBoard_DTO> list = salboardDao.saleslist(page);
+	model.addAttribute("list", list);
+	//System.out.println(pg+" / "+f+" / "+q);
 	
+	return "salesboard/salboardlist";
+}
+	
+		//판매글등록
+		 @RequestMapping(value="salboardwrite.five" , method=RequestMethod.GET)
+		 public String salboardReg(){
+				return "salesboard/salboardwrite";
+		  }
+
+	 	//판매글등록 처리(실제 글등록 처리)
+	 	@RequestMapping(value="salboardwrite.five",method=RequestMethod.POST)
+	 	public String salboardReg(SalesBoard_DTO n, HttpServletRequest request) throws IOException, ClassNotFoundException, SQLException{
+				
+		 			CommonsMultipartFile file =n.getFile();
+		 			System.out.println(file);
+		 			String fname = file.getOriginalFilename();
+			       if(file != null){
+					String path = request.getServletContext().getRealPath("/salesboard/upload");
+					String fullpath = path + "\\" + fname;
+					
+					if(!fname.equals("")){
+						//서버에 물리적 경로 파일쓰기작업
+						FileOutputStream fs = new FileOutputStream(fullpath);
+						fs.write(file.getBytes());
+						fs.close();
+					}
+			       }
+			      
+			       System.out.println(fname);
+			       SalesBoard_Dao salboardDao= sqlSession.getMapper(SalesBoard_Dao.class);
+					n.setBo_photo(fname);
+				  //n.setUser_id(principal.getName());
+					salboardDao.insert(n);
+					return "redirect:salboardlist.five"; //요청 주소
+	 		}	
+	
+	 	//판매글상세보기
+ 		@RequestMapping("salesdetail.five")
+ 		public String salesdetail(String bo_num, Model model) throws ClassNotFoundException, SQLException{
+ 		System.out.println("상세보기 controller 진입");
+ 		SalesBoard_Dao salboardDao= sqlSession.getMapper(SalesBoard_Dao.class);
+ 		SalesBoard_DTO salboardDto = salboardDao.salesdetail(bo_num);
+ 		System.out.println("mapper소환 Dto에 bo_num");
+		 model.addAttribute("salboardDto", salboardDto);
+		 
+		 //review 목록
+		 //List<B_reply_DTO> list = boardDao.re_list(idx);
+		 //model.addAttribute("list", list);
+		 
+		 return "salesboard/salboarddetail"; 
+	 }
+
+ 		 //수정 기존데이터 출력
+		 @RequestMapping(value="salboardedit.five",method=RequestMethod.GET)
+			public String salboardedit(String bo_num, Model model) throws ClassNotFoundException, SQLException{
+			 	SalesBoard_Dao salboardDao= sqlSession.getMapper(SalesBoard_Dao.class);
+		 		SalesBoard_DTO salboardDto = salboardDao.salesdetail(bo_num);
+			 	model.addAttribute("salboardDto", salboardDto);
+			 	return "salesboard/salboardedit";
+			}
+		 
+		//수정 실행문
+		@RequestMapping(value={"salboardedit.five"},method=RequestMethod.POST)   //=>customer/notice.htm
+		public String salboardedit(SalesBoard_DTO n, HttpServletRequest request) throws ClassNotFoundException, SQLException, IOException{
+			   
+					CommonsMultipartFile file =n.getFile();
+					System.out.println(file);
+					String fname = file.getOriginalFilename();
+				if(!n.getFile().isEmpty()){
+					String path = request.getServletContext().getRealPath("/salesboard/upload");
+					String fpath = path + "\\" + fname;
+					//파일쓰기 작업
+					FileOutputStream fs = new FileOutputStream(fpath);
+					fs.write(n.getFile().getBytes());
+					fs.close();
+					n.setBo_photo(fname); //파일이름 
+				}
+				SalesBoard_Dao salboardDao= sqlSession.getMapper(SalesBoard_Dao.class);
+				salboardDao.update(n);	
+		   	   return "redirect:salesdetail.five?bo_num="+n.getBo_num();
+			}
+		 
+		//판매글삭제하기
+		 @RequestMapping("salboarddelete.five") 
+		public String salboardDel(String bo_num) throws ClassNotFoundException, SQLException{
+			 SalesBoard_Dao salboardDao= sqlSession.getMapper(SalesBoard_Dao.class);
+			 salboardDao.delete(bo_num);
+			return "redirect:salboardlist.five";
+		}  
+		 
+	 @RequestMapping("unitlist.five")  
+	 public void unitlist(HttpServletResponse response) throws IOException{
+		 SalesBoard_Dao salboardDao= sqlSession.getMapper(SalesBoard_Dao.class);
+		 List<Unit_DTO> list = salboardDao.unitlist();
+		 JSONArray unitlists = JSONArray.fromObject(list);
+         response.getWriter().print(unitlists);//서버로 데이터 전송
+         System.out.println("서버로 list 전송완료");
+	 }
+
 
 	// 테스트용 페이지 이동
 	@RequestMapping("test.five")
---
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/vo/Unit_DTO.java
@@ -0,0 +1,14 @@
+package kr.co.ohdeokrionline.model.vo;
+
+public class Unit_DTO {
+	String unit;
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+		
+}
