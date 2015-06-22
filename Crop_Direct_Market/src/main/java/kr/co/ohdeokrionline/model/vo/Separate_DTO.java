85aaaca4b00112b3ba49808a94c6cfde7050ecd3
Kihee Sung <tjdrlgml@gmail.com>
2015-06-22 11:11:45 +0900

    commit product, separate DTO

---
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/vo/Separate_DTO.java
@@ -0,0 +1,20 @@
+package kr.co.ohdeokrionline.model.vo;
+
+public class Separate_DTO {
+	String pro_id;
+	String pro_sep;
+	
+	public String getPro_id() {
+		return pro_id;
+	}
+	public void setPro_id(String pro_id) {
+		this.pro_id = pro_id;
+	}
+	public String getPro_sep() {
+		return pro_sep;
+	}
+	public void setPro_sep(String pro_sep) {
+		this.pro_sep = pro_sep;
+	}
+	
+}
