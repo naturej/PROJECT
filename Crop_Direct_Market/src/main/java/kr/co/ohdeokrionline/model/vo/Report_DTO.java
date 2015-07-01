18b7693fc3b274713858c6d7af028488c097ccf2
naturej <natu_rej@naver.com>
2015-07-01 09:54:29 +0900

    신고 DTO

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/vo/Report_DTO.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/vo/Report_DTO.java
@@ -1,5 +1,53 @@
 package kr.co.ohdeokrionline.model.vo;
 
+import java.sql.Date;
+
+// 신고 테이블
 public class Report_DTO {
+	private String user_send;	// 신고자
+	private int bo_num;			// 게시글 번호
+	private String user_rec;	// 피신고자
+	private String rep_content;	// 신고내용
+	private Date rep_date;		// 신고날짜
+
+	public Report_DTO(String user_send, int bo_num, String user_rec,
+			String rep_content, Date rep_date) {
+		this.user_send = user_send;
+		this.bo_num = bo_num;
+		this.user_rec = user_rec;
+		this.rep_content = rep_content;
+		this.rep_date = rep_date;
+	}
+	
+	public String getUser_send() {
+		return user_send;
+	}
+	public void setUser_send(String user_send) {
+		this.user_send = user_send;
+	}
+	public int getBo_num() {
+		return bo_num;
+	}
+	public void setBo_num(int bo_num) {
+		this.bo_num = bo_num;
+	}
+	public String getUser_rec() {
+		return user_rec;
+	}
+	public void setUser_rec(String user_rec) {
+		this.user_rec = user_rec;
+	}
+	public String getRep_content() {
+		return rep_content;
+	}
+	public void setRep_content(String rep_content) {
+		this.rep_content = rep_content;
+	}
+	public Date getRep_date() {
+		return rep_date;
+	}
+	public void setRep_date(Date rep_date) {
+		this.rep_date = rep_date;
+	}
 
 }
