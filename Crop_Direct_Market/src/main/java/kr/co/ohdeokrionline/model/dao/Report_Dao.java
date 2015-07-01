084d1246edbe7b0f63c0b7e405e29d401bed0ed9
naturej <natu_rej@naver.com>
2015-07-01 10:20:27 +0900

    신고 DAO

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/dao/Report_Dao.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/dao/Report_Dao.java
@@ -1,5 +1,8 @@
 package kr.co.ohdeokrionline.model.dao;
 
-public interface Report_Dao {
+import kr.co.ohdeokrionline.model.vo.Report_DTO;
 
+public interface Report_Dao {
+		// 신고 작성 
+		public void reportInsert(Report_DTO report);
 }
