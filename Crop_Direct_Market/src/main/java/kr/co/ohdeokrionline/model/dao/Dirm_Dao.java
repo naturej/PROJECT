e5afb6b79ec8d3f52cfc1ab0498052da64ead5ef
naturej <natu_rej@naver.com>
2015-07-02 19:42:14 +0900

    Dirm 고치는중! >_<

--- Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/dao/Dirm_Dao.java
+++ Crop_Direct_Market/src/main/java/kr/co/ohdeokrionline/model/dao/Dirm_Dao.java
@@ -1,5 +1,16 @@
 package kr.co.ohdeokrionline.model.dao;
 
-public interface Dirm_Dao {
+import java.util.ArrayList;
 
+import kr.co.ohdeokrionline.model.vo.Dirm_DTO;
+
+public interface Dirm_Dao {
+	// 직거래 쪽지 목록
+	public ArrayList<Dirm_DTO> dirmList(String user_rec);
+	// 직거래 신청
+	public void insertDirm(Dirm_DTO dirm);
+	// 직거래 상세보기
+	public Dirm_DTO dirmDetail(String dirmno);
+	// 직거래 승인
+	public void yesDirm(String dirmno);
 }
